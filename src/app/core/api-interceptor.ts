d920e76787ecbb095e69ec6b1463ff3b32e4546d
Bryan Fernandez <brayanzabaleta59@gmail.com>
2024-11-14 01:13:22 -0500

    Auto complete deshabilitado para cajas de login

--- src/app/core/api-interceptor.ts
+++ src/app/core/api-interceptor.ts
@@ -3,11 +3,16 @@ import { Injectable } from "@angular/core";
 import { catchError, Observable, throwError } from "rxjs";
 import { AuthService } from "./services/auth.service";
 import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
 
-    constructor(private authService: AuthService, private router: Router) {}
+    constructor(
+        private authService: AuthService, 
+        private router: Router,
+        private toastrService: ToastrService,
+    ) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const accessToken = this.authService.getAccessToken();
@@ -19,6 +24,7 @@ export class ApiInterceptor implements HttpInterceptor {
                 catchError((error) => {
                     if(error.status === 401) {
                         this.authService.clearUserSession();
+                        this.toastrService.warning("Su sesion expiro");
                         this.router.navigate(['login']);
                     }
                     return throwError(error);
